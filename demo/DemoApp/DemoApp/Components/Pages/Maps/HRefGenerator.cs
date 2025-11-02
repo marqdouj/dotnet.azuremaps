68b5b1aa09085f158ddb11d9aaddd83fb0bfb9f5
Douglas Marquardt <16510416+marqdouj@users.noreply.github.com>
2025-11-02 06:13:01 -0700

    DemoApp: update source/code links

--- demo/DemoApp/DemoApp/Components/Pages/Maps/HRefGenerator.cs
+++ demo/DemoApp/DemoApp/Components/Pages/Maps/HRefGenerator.cs
@@ -1,6 +1,8 @@
-﻿namespace DemoApp.Components.Pages.Maps
+﻿using Marqdouj.DotNet.AzureMaps.Map.Interop.Layers;
+
+namespace DemoApp.Components.Pages.Maps
 {
-    internal enum HRefCodeSource
+    internal enum HRefSource
     {
         AzureMaps,
         DemoApp,
@@ -10,14 +12,45 @@
 
     internal static class HRefGenerator
     {
-        public static string CodeUrl(this HRefCodeSource source, string path)
+        public static string? ToHRefAzureDocs(this MapLayerDef? layerDef) => layerDef?.Type.ToHRefAzureDocs();
+
+        public static string ToHRefAzureDocs(this MapLayerType layerType)
+        {
+            string codePath = layerType switch
+            {
+                MapLayerType.Bubble => "map-add-bubble-layer",
+                MapLayerType.HeatMap => "map-add-heat-map-layer",
+                MapLayerType.Image => "map-add-image-layer",
+                MapLayerType.Line => "map-add-line-layer",
+                MapLayerType.Polygon => "map-add-shape",
+                MapLayerType.PolygonExtrusion => "map-extruded-polygon",
+                MapLayerType.Symbol => "map-add-pin",
+                MapLayerType.Tile => "map-add-tile-layer",
+                _ => throw new NotImplementedException(),
+            };
+
+            var hRef = HRefSource.AzureDocs.CodeUrl(codePath);
+            return hRef;
+        }
+
+        public static string? ToHRefAddLayerExample(this MapLayerDef? layerDef) => layerDef?.Type.ToHRefAddLayerExample();
+
+        public static string ToHRefAddLayerExample(this MapLayerType layerType)
+        {
+            var hRef = HRefSource.Examples.CodeUrl($"add{layerType}Layer.md");
+            return hRef;
+        }
+
+        public static string ToPageSource(this string name) => HRefSource.DemoApp.CodeUrl($"{name}.razor");
+
+        public static string CodeUrl(this HRefSource source, string path)
         {
             string? url = source switch
             {
-                HRefCodeSource.AzureDocs => "https://learn.microsoft.com/en-us/azure/azure-maps",
-                HRefCodeSource.AzureMaps => "https://github.com/marqdouj/dotnet.azuremaps/blob/master/src/Marqdouj.DotNet.AzureMaps/Marqdouj.DotNet.AzureMaps",
-                HRefCodeSource.DemoApp => "https://github.com/marqdouj/dotnet.azuremaps/blob/master/demo/DemoApp/DemoApp/Components/Pages/Maps",
-                HRefCodeSource.Examples => "https://github.com/marqdouj/dotnet.azuremaps/blob/master/docs/examples/",
+                HRefSource.AzureDocs => "https://learn.microsoft.com/en-us/azure/azure-maps",
+                HRefSource.AzureMaps => "https://github.com/marqdouj/dotnet.azuremaps/blob/master/src/Marqdouj.DotNet.AzureMaps/Marqdouj.DotNet.AzureMaps",
+                HRefSource.DemoApp => "https://github.com/marqdouj/dotnet.azuremaps/blob/master/demo/DemoApp/DemoApp/Components/Pages/Maps",
+                HRefSource.Examples => "https://github.com/marqdouj/dotnet.azuremaps/blob/master/docs/examples/",
                 _ => throw new NotImplementedException(),
             };
             return Path.Combine(url, path);
