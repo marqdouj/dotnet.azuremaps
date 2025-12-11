d2d4942459cf83c4e13a3df9d7c9cc7a4c5d500b
Douglas Marquardt <16510416+marqdouj@users.noreply.github.com>
2025-12-11 16:54:56 -0700

    migrate map control methods to 'controls.ts'

---
+++ src/MainLib/Marqdouj.DotNet.AzureMaps/scripts/map-interop/controls.ts
@@ -0,0 +1,103 @@
+import * as atlas from "azure-maps-control";
+import { MapFactory } from "../map-factory"
+import { Logger } from "../common"
+import { MapControl } from "../typings"
+
+export class Controls {
+    public static addControls(mapId: string, controls: MapControl[]): void {
+        const map = MapFactory.getMap(mapId);
+        if (!map) {
+            return;
+        }
+
+        controls.forEach(control => {
+            let mapControl: atlas.Control = null;
+
+            switch (control.type.toLowerCase()) {
+                case "compass":
+                    mapControl = new atlas.control.CompassControl(control.options as atlas.CompassControlOptions);
+                    break;
+                case "fullscreen":
+                    mapControl = new atlas.control.FullscreenControl(control.options as atlas.FullscreenControlOptions);
+                    break;
+                case "pitch":
+                    mapControl = new atlas.control.PitchControl(control.options as atlas.PitchControlOptions);
+                    break;
+                case "scale":
+                    mapControl = new atlas.control.ScaleControl(control.options as atlas.ScaleControlOptions);
+                    break;
+                case "style":
+                    mapControl = new atlas.control.StyleControl(control.options as atlas.StyleControlOptions);
+                    break;
+                case "zoom":
+                    mapControl = new atlas.control.ZoomControl(control.options as atlas.ZoomControlOptions);
+                    break;
+                default:
+                    Logger.logMessage(mapId, Logger.LogLevel.Warn, `- addControls: control type '${control.type}' is not supported.`);
+                    break;
+            }
+
+            if (mapControl) {
+                const jsInterop: JsInteropControl = {
+                    id: control.id,
+                    interopId: control.interopId,
+                    type: control.type,
+                    options: control.options,
+                    controlOptions: control.controlOptions,
+                };
+
+                (mapControl as any).jsInterop = jsInterop;
+
+                map.controls.add(mapControl, control.controlOptions);
+            }
+        });
+    }
+
+    public static getControls(mapId: string): object[] {
+        const map = MapFactory.getMap(mapId);
+        if (!map) {
+            return;
+        }
+
+        const controls = map.controls.getControls();
+        const result: JsInteropControl[] = [];
+
+        controls.filter(control => this.#isInteropControl(control)).forEach(control => {
+            const jsInterop = (control as any).jsInterop as JsInteropControl;
+            if (jsInterop)
+                result.push(jsInterop);
+        });
+
+        return result;
+    }
+
+    public static removeControls(mapId: string, controls: MapControl[]) {
+        const map = MapFactory.getMap(mapId);
+        if (!map) {
+            return;
+        }
+
+        const mapControls = map.controls.getControls();
+
+        mapControls.filter(control => this.#isInteropControl(control)).forEach(control => {
+            const jsInterop = (control as any).jsInterop as JsInteropControl;
+            if (jsInterop) {
+                if (controls.find(e => e.id === jsInterop.id || e.interopId === jsInterop.interopId)) {
+                    map.controls.remove(control);
+                }
+            }
+        });
+    }
+
+    static #isInteropControl(obj: any): obj is atlas.Control {
+        return obj && obj.jsInterop != undefined && obj.jsInterop.interopId != undefined;
+    }
+}
+
+interface JsInteropControl {
+    id: string,
+    interopId: string,
+    type: string,
+    options: object;
+    controlOptions: object;
+}
--- src/MainLib/Marqdouj.DotNet.AzureMaps/scripts/map-interop/maps.ts
+++ src/MainLib/Marqdouj.DotNet.AzureMaps/scripts/map-interop/maps.ts
@@ -3,6 +3,7 @@ import { MapFactory } from "../map-factory"
 import { Logger } from "../common"
 import { MapControl, DataSourceDef } from "../typings"
 import { EventManager, MapEvent } from "../map-events"
+import { Controls } from "./controls"
 
 export class Maps {
     static addEvents(dotNetRef: any, mapId: string, events: MapEvent[]): void {
@@ -14,89 +15,15 @@ export class Maps {
     }
 
     public static addControls(mapId: string, controls: MapControl[]): void {
-        const map = MapFactory.getMap(mapId);
-        if (!map) {
-            return;
-        }
-
-        controls.forEach(control => {
-            let mapControl: atlas.Control = null;
-
-            switch (control.type.toLowerCase()) {
-                case "compass":
-                    mapControl = new atlas.control.CompassControl(control.options as atlas.CompassControlOptions);
-                    break;
-                case "fullscreen":
-                    mapControl = new atlas.control.FullscreenControl(control.options as atlas.FullscreenControlOptions);
-                    break;
-                case "pitch":
-                    mapControl = new atlas.control.PitchControl(control.options as atlas.PitchControlOptions);
-                    break;
-                case "scale":
-                    mapControl = new atlas.control.ScaleControl(control.options as atlas.ScaleControlOptions);
-                    break;
-                case "style":
-                    mapControl = new atlas.control.StyleControl(control.options as atlas.StyleControlOptions);
-                    break;
-                case "zoom":
-                    mapControl = new atlas.control.ZoomControl(control.options as atlas.ZoomControlOptions);
-                    break;
-                default:
-                    Logger.logMessage(mapId, Logger.LogLevel.Warn, `- addControls: control type '${control.type}' is not supported.`);
-                    break;
-            }
-
-            if (mapControl) {
-                (mapControl as any).jsInterop = {
-                    id: control.id,
-                    interopId: control.interopId,
-                    type: control.type,
-                    options: control.controlOptions
-                };
-
-                map.controls.add(mapControl, control.controlOptions);
-            }
-        });
+        Controls.addControls(mapId, controls);
     }
 
     public static getControls(mapId: string): object[] {
-        const map = MapFactory.getMap(mapId);
-        if (!map) {
-            return;
-        }
-
-        const controls = map.controls.getControls();
-        const result: object[] = [];
-
-        controls.filter(control => this.#isInteropControl(control)).forEach(control => {
-            const custom = control as any;
-            const item: object = {
-                id: custom.jsInterop.id,
-                interopId: custom.jsInterop.interopId,
-                type: custom.jsInterop.type,
-                options: custom.jsInterop.options
-            };
-            result.push(item);
-        });
-
-        return result;
+        return Controls.getControls(mapId);
     }
 
-    public static removeControls(mapId: string, controls: MapControl[]) {
-        const map = MapFactory.getMap(mapId);
-        if (!map) {
-            return;
-        }
-
-        const mapControls = map.controls.getControls();
-
-        mapControls.filter(control => this.#isInteropControl(control)).forEach(control => {
-            const custom = control as any;
-
-            if (controls.find(e => e.id === custom.jsInterop.id || e.interopId === custom.jsInterop.interopId)) {
-                map.controls.remove(control);
-            }
-        });
+    public static removeControls(mapId: string, controls: MapControl[]): void {
+        Controls.removeControls(mapId, controls);
     }
 
     public static clearDatasource(mapId: string, id: string): void {
@@ -143,9 +70,5 @@ export class Maps {
             map.sources.remove(ds);
             Logger.logMessage(mapId, Logger.LogLevel.Debug, `- removeDatasource: datasource with ID '${id}' was removed.`);
         }
-    }
-
-    static #isInteropControl(obj: any): obj is atlas.Control {
-        return obj && obj.jsInterop != undefined;
     }
 }
