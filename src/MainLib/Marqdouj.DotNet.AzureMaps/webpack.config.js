d28ba488d5fd0b7e27a671cbcb6d5d8e7453d115
Douglas Marquardt <marqdouj@users.noreply.github.com>
2026-01-08 05:26:19 -0700

    Reconfigure how TS builds *.js and WebPack bundles it. Switch *.csproj to use CoreCompile target.

--- src/MainLib/Marqdouj.DotNet.AzureMaps/webpack.config.js
+++ src/MainLib/Marqdouj.DotNet.AzureMaps/webpack.config.js
@@ -2,24 +2,10 @@
 
 module.exports = {
     mode: 'production',
-    entry: './scripts/index.ts',
-    module: {
-        rules: [
-            // all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
-            {
-                test: /\.([cm]?ts|tsx)$/,
-                loader: "ts-loader",
-                exclude: /node_modules/,
-            }
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
+    entry: './tsgen/index.js',
     output: {
         filename: 'marqdouj-azuremaps.js',
         path: path.resolve(__dirname, 'wwwroot'),
-        clean: true,
         library: 'marqdoujAzureMaps',
     },
     externals: {
